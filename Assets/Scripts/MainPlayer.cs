5196aa8098f3b9250cd0797411d643783f4f0145
NolanEperjesi <143567008+NolanEperjesi@users.noreply.github.com>
2025-03-26 19:58:59 -0500

    Minor update to player movement

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -5,15 +5,12 @@ public class MainPlayer : MonoBehaviour
 {
     public float movementSpeed = 15.0f;
 
-    
-
-
     Rigidbody rb;
-
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
