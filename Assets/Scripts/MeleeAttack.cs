34ce45b57aaa36117bd29d1e313ec04c3c004079
Car1010-10 <06karol.hernan@gmail.com>
2025-04-08 19:07:38 -0500

    Melee Attack updated

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -1,16 +1,48 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MeleeAttack : WeaponBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float damageAmount = 10.0f;
+    public float attackRange = 2f;
+    float lastAttackTime;
+
+    protected override void ChildStart()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void ChildUpdate()
     {
-        
+
+    }
+
+    public override void PerformAttack()
+    {
+        Debug.Log("Melee Attack - Perform Attack!");
+
+        //RaycastHit hit;
+        //bool hasHit = Physics.Raycast(ProjectileSpawnPoint.transform.position, BarrelEnd.transform.forward, out hit, RaycastDistance);
+
+        //Instantiate(HitMarkerPrefab, hit.point, Quaternion.Euler(hit.normal));
+
+        /*
+        RobotPlayerPW cp = hit.collider.gameObject.GetComponentInParent<CombatPlayer>();
+        if (cp)
+        {
+            Destroy(gameObject);
+
+            string KillMessage = gameObject.name + " hit " + cp.gameObject.name;
+            Debug.Log(KillMessage);
+            return;
+        }
+
+        RobotPlayerPW RangeAttack = hit.collider.gameObject.GetComponentInParent<RobotPlayerPW>();
+        if (RangeAttack)
+        {
+            Destroy(RangeAttack.gameObject);
+        }
+        */
     }
 }
+
