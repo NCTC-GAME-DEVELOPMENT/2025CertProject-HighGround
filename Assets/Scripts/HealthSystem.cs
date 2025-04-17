43c5b7106bda2d426ea2a0c042cadda3708d600d
NolanEperjesi <06karol.hernan@gmail.com>
2025-04-16 21:01:18 -0500

    added health system
    This has to look for a health system

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -4,19 +4,54 @@ using UnityEngine.Events;
 public class HealthSystem : MonoBehaviour
 {
     public float currentHealth;
-    public float maxHealth;
+    public float maxHealth = 100f;
+    public UnityEvent OnDeath;
+    public UnityEvent OnTakeDamage;
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void TakeDamage(float damageValue)
+    {
+        currentHealth -= damageValue;
+        if (currentHealth < 0)
+        {
+            OnDeath?.Invoke();
+            return; 
+        }    
+
+        OnTakeDamage?.Invoke(); 
+    }
+
+    public void onDamagedTest()
+    {
+        Debug.Log(gameObject.name + " has been damaged");
+
+    }
+
+    public void DeathEventTest()
+    {
+        Debug.Log(gameObject.name + " is destroyed");
+        Destroy(gameObject);
+    }
+
+
+
+
+
+
+
+
+
 }
