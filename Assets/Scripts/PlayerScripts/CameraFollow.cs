47dc9d065eb7f36d014ff1787f127c4a75208225
NolanEperjesi <06karol.hernan@gmail.com>
2025-04-30 18:35:42 -0500

    Camera follow is now working

--- Assets/Scripts/PlayerScripts/CameraFollow.cs
+++ Assets/Scripts/PlayerScripts/CameraFollow.cs
@@ -1,18 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class CameraFollow : MonoBehaviour
 {
     public GameObject cameraRoot;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = transform.position;
+        gameObject.transform.position = cameraRoot.transform.position;
     }
 }
